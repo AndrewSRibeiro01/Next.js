e30a4f0207deebad69bd9e431b027cfa82a3b2ac
AndrewSRB <116013992+AndrewSRibeiro01@users.noreply.github.com>
2023-05-27 14:27:42 -0300

    Atualizando o projeto

--- Next.js/src/components/Link/faq.js
+++
@@ -1,8 +0,0 @@
-export default function FAQPage() {
-    return (
-        <div>
-            <h1>Alura Cases - Páginas de Perguntas FAQ</h1>
-            <a href="/">Ir para a home</a>
-        </div>
-    )
-}
